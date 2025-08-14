54fe47583e1d7c19a7fd83161622f0b45f929dcf
Arman Mir <mirarman8583@gmail.com>
2025-08-14 08:18:50 +0600

    math

---
+++ math.js
@@ -0,0 +1,73 @@
+// const min = Math.min;
+// const max = Math.max;
+// const abs = Math.abs;
+// const sqrt = Math.sqrt;
+// const pow = Math.pow;
+// const round = Math.round;
+// const floor = Math.floor;
+// const ceil = Math.ceil;
+// const random = Math.random;
+// const PI = Math.PI;
+// const E = Math.E;
+// const LN2 = Math.LN2;
+// const LOG2E = Math.LOG2E;
+// const LOG10E = Math.LOG10E;
+// const SQRT1_2 = Math.SQRT1_2;
+// const SQRT2 = Math.SQRT2;   
+// const sign = Math.sign;
+// const trunc = Math.trunc;
+// const fround = Math.fround;
+// const hypot = Math.hypot;
+// const imul = Math.imul;
+// const clz32 = Math.clz32;
+// const exp = Math.exp;
+// const log = Math.log;
+// const log1p = Math.log1p;
+// const cos = Math.cos;   
+// const sin = Math.sin;
+// const tan = Math.tan;
+// const acos = Math.acos;
+// const asin = Math.asin;
+// const atan = Math.atan;
+// const atan2 = Math.atan2;
+// const cosh = Math.cosh;
+// const sinh = Math.sinh;
+// const tanh = Math.tanh;
+// const cbrt = Math.cbrt;
+// const log10 = Math.log10;
+// const log2 = Math.log2;
+// const scale = Math.scale;
+
+
+// const math  = {
+//     min:function min(num1, num2) {
+//     return num2;}
+// }
+
+
+
+console.log(Math.min); 
+console.log(Math.max);
+
+const min = Math.min(45 , 2 , 564 , 545 , -5+4646464,  45 , 1 , 5454 , 4544 , 545 , 45 , 1 , 5454 , 4544);
+console.log('Minimum value is:', min);
+
+const max = Math.max(45 , 2 , 564 , 545 , 45 , 1 , 5454 , 4544 , 545 , 45 , 1 , 5454 , 4544);
+console.log('Maximum value is:', max);
+
+console.log(Math.PI);
+
+console.log(Math.abs(-45)); // Makes a number positive
+
+console.log(Math.round(4.4)); // Rounds to the nearest integer
+
+console.log(Math.floor(4.99999999999)); // Rounds down to the nearest integer
+
+console.log(Math.ceil(4.00000000001)); // Rounds up to the nearest integer
+
+console.log(Math.random()*1500); // Generates a random number between 0 and 1
+
+
+console.log(Math.random()*10);
+const rand =Math.floor(Math.random()*100); // Generates a random number between 0 and 99
+console.log('Random number is:', rand);
